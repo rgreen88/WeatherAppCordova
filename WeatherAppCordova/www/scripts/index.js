81899fe7a78c4dd0b115e234c33fc5940c62f06f
rgreen88 <rynelgreen@gmail.com>
2019-07-30 15:38:58 -0400

    created weather.js and added openweather api key for weather button funxctionality

--- WeatherAppCordova/www/scripts/index.js
+++ WeatherAppCordova/www/scripts/index.js
@@ -7,17 +7,14 @@
 
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
-    function onDeviceReady() {
+    function onDeviceReady() //Can call Cordova APIs after onDeviceReady completes
+    {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+
+        // Get Weather button adding functionality via click listener executing getWeatherWithZipCode each click
+        $('#get-weather-btn').click(getWeatherWithZipCode);
     };
 
     function onPause() {
---
+++ WeatherAppCordova/www/scripts/weather.js
@@ -0,0 +1,42 @@
+﻿var OpenWeatherAppKey = "dd80f3c439caf6d2ddb6323ebc468a92"; //openweathermap api key
+
+function getWeatherWithZipCode() //Click listener for activating weather retrieval when hitting get weather button
+{
+    var zipcode = $('#zip-code-input').val();
+    var queryString =
+        'http://api.openweathermap.org/data/2.5/weather?zip='
+        + zipcode + ',us&appid=' + OpenWeatherAppKey + '&units=imperial';
+    $.getJSON(queryString, function (results) {
+        showWeatherData(results);
+    }).fail(function (jqXHR) {
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. " + jqXHR.statusText);
+    });
+    return false;
+}
+
+//updating current page (index.html) with weather conditions in UI elements
+function showWeatherData(results) {
+
+    if (results.weather.length) {
+        $('#error-msg').hide();
+        $('#weather-data').show();
+
+        $('#title').text(results.name);
+        $('#temperature').text(results.main.temp);
+        $('#wind').text(results.wind.speed);
+        $('#humidity').text(results.main.humidity);
+        $('#visibility').text(results.weather[0].main);
+
+        var sunriseDate = new Date(results.sys.sunrise * 1000);
+        $('#sunrise').text(sunriseDate.toLocaleTimeString());
+
+        var sunsetDate = new Date(results.sys.sunset * 1000);
+        $('#sunset').text(sunsetDate.toLocaleTimeString());
+
+    } else {
+        $('#weather-data').hide();
+        $('#error-msg').show();
+        $('#error-msg').text("Error retrieving data. ");
+    }
+}
